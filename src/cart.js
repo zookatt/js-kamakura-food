b2007b95073e01b4b3d8795a5f52225093c0bc91
zookat <zotova.ea@gmail.com>
2026-05-06 11:26:46 +0200

    add: products cuantity and total

--- src/cart.js
+++ src/cart.js
@@ -25,10 +25,23 @@ function paintCart() {
             <h3>${product.name}</h3>
             <h5>${product.price} €</h5>
           </div>
+          <div class="quantity-container">
+  <button class="add-quantity" data-id="${product.id}">+</button>
+  <p class="quantity">${product.quantity}</p>
+  <button class="rest-quantity" data-id="${product.id}">-</button>
+</div>
         </div>
       `,
     )
     .join("");
+ // 
+  const cartTotal = document.getElementById("cart-total");
+
+  const total = cart.reduce((acc, product) => {
+    return acc + product.price * product.quantity;
+  }, 0);
+
+  cartTotal.textContent = `Total: ${total.toFixed(2)} €`;
 }
 
 //DEBE contener las funcionalidades del carrito de compras.
@@ -59,13 +72,30 @@ export function initCart() {
       return;
     }
 
-    cart.push(productFound);
+    cart.push({
+      ...productFound,
+      quantity: 1,
+    });
     paintCart();
   });
 
   const cartProductsContainer = document.getElementById("products-container");
 
   cartProductsContainer.addEventListener("click", (event) => {
+    const addButton = event.target.closest(".add-quantity");
+    if (addButton) {
+      const productId = Number(addButton.dataset.id);
+      addQuantity(productId);
+      return;
+    }
+
+    const restButton = event.target.closest(".rest-quantity");
+    if (restButton) {
+      const productId = Number(restButton.dataset.id);
+      restQuantity(productId);
+      return;
+    }
+
     const button = event.target.closest(".close-button");
     if (!button) return;
 
@@ -77,5 +107,37 @@ export function initCart() {
       paintCart();
     }
   });
+  paintCart();
+}
+
+// El plato del carrito de compras lleva un contador, has de crear la función que recoja la cantidad
+// escogida y haga el cálculo para obtener el subtotal por cada plato y luego que imprima el total
+// de todos los platos en pantalla, dentro del texto que dice "Total: €". Cuando el contador llegue
+// a 0 deberá desaparecer el plato del carrito de compras.
+
+function addQuantity(productId) {
+  const product = cart.find((item) => item.id === productId);
+
+  if (!product) {
+    return;
+  }
+
+  product.quantity += 1;
+  paintCart();
+}
+
+function restQuantity(productId) {
+  const index = cart.findIndex((item) => item.id === productId);
+
+  if (index === -1) {
+    return;
+  }
+
+  cart[index].quantity -= 1;
+
+  if (cart[index].quantity === 0) {
+    cart.splice(index, 1);
+  }
+
   paintCart();
 }
